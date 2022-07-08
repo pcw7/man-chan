0f76f97ab45ccfd69b415f721f343ce2a11703a9
parkc <parkc@DESKTOP-CNKO5EO>
2022-07-09 02:55:40 +0900

    회원가입 기본??

---
+++ man-chan-backend/src/main/java/com/ch/mc/model/Member.java
@@ -0,0 +1,13 @@
+package com.ch.mc.model;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private String id;        // 아이디
+	private String password;  // 비밀번호
+	private String email;     // 이메일
+	private String nickName;  // 별명
+}
